e196b3f728715927e7a0f9afe07078c22ce6b2b7
Michael Hainke <hainke@hotmail.com>
2022-12-15 22:35:54 -0800

    Update Day 10

--- Day10.R
+++ Day10.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 
 commands <- str_split(readLines("day10_input.txt")," ")
-commands <- str_split(readLines("day10_test_input.txt")," ")
 
 X <- 1
 
@@ -14,11 +13,11 @@ for (row in commands) {
 sum(X[20]*20,X[60]*60,X[100]*100,X[140]*140,X[180]*180,X[220]*220)
 
 # Part 2: render image
-Y <- NULL
-for (crt in 0:239) {
-  if (abs(X[crt+1]-crt)<=1) { Y <- c(Y,'#')} else { Y <- c(Y, '.')}
+display <- NULL
+for (line in seq(from=1,to=240,by=40)) {
+  Y <- NULL
+  for (crt in 0:39) {
+    if (abs(X[line+crt]-crt)<=1) { Y <- paste0(Y,'#')} else { Y <- paste0(Y, '.')}
+  }
+  if (is.null(display)) { display <- Y } else { display <- rbind(display,Y) }
 }
-
-display <- as.data.frame(rbind(Y[1:40],Y[41:80],substring(Y,81,120),substring(Y,121,160),substring(Y,161,200),substring(Y,201,240)))
-
-substring(Y,1,40)
